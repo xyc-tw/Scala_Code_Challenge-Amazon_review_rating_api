9e08b6931de286d4268d4aeaf76edc0c7e3ce212
xyc-tw <53919939+xyc-tw@users.noreply.github.com>
2023-05-10 08:17:54 +0200

    Add files via upload
    refactory

--- ApiServer.scala
+++ ApiServer.scala
@@ -9,8 +9,7 @@ import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions._
 
 // Libraries for json
-import io.circe.{ Decoder, Encoder, Json }
-import io.circe.parser.parse
+import io.circe.{ Decoder, Json }
 import io.circe.parser.decode
 import io.circe.generic.semiauto._
 
@@ -19,19 +18,19 @@ object ApiServer {
   case class Review(asin: String, helpful: Array[Long], overall: Double, reviewText: String, reviewerID: String, reviewerName: String, summary: String, unixReviewTime: Long)
 
   /** Transfer the input StringTime to UnixTime */
-  def stringToUnixtime(pos: String, stringTime: String): Long = {
+  private def stringToUnixTime(pos: String, stringTime: String): Long = {
     val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-    val unixtime = LocalDate.parse(stringTime, formatter).atStartOfDay(ZoneOffset.UTC).toEpochSecond
+    val unixTime = LocalDate.parse(stringTime, formatter).atStartOfDay(ZoneOffset.UTC).toEpochSecond
 
-    if (pos == "start") unixtime else unixtime + 86400
+    if (pos == "start") unixTime else unixTime + 86400
   }
 
   /** Filter data according to the request */
-  def filter_data(ds: Dataset[Review], start: String, end: String, limit: Int, min_number_reviews: Int): String = {
+  private def filter_data(ds: Dataset[Review], start: String, end: String, limit: Int, min_number_reviews: Int): String = {
 
     /**  Step 1: filter time */
-    val timeFiltered = ds.filter(review => review.unixReviewTime >= stringToUnixtime("start", start)
-      && review.unixReviewTime < stringToUnixtime("end", end))
+    val timeFiltered = ds.filter(review => review.unixReviewTime >= stringToUnixTime("start", start)
+      && review.unixReviewTime < stringToUnixTime("end", end))
 
     /**  Step 2: group by review id and count the average */
     val asinGrouped = timeFiltered.groupBy("asin")
@@ -45,7 +44,7 @@ object ApiServer {
 
     /**  Step 5: convert result dataset to JSON */
     val resultJson = resultDf.select(col("asin"), col("average_rating"))
-      .toJSON.collect().mkString("[", ",", "]")
+      .toJSON.collect().mkString("[", ",", "]") //DataFrame -> RDD[String] -> Array[String] -> String
 
     resultJson
   }
@@ -85,30 +84,28 @@ object ApiServer {
             val path = exchange.getRequestURI.getPath
             path match {
               case "/" =>
-                val jsonBody = scala.io.Source.fromInputStream(exchange.getRequestBody).mkString // input stream->String
-                val json = parse(jsonBody).getOrElse(Json.Null).toString() // String -> io.circe.Json -> String
-                val result = decode[RequestBody](json) // json String -> RequestBody
+                val jsonBody = scala.io.Source.fromInputStream(exchange.getRequestBody).mkString // input stream -> scala.io.BufferedSource -> String
+                val result = decode[RequestBody](jsonBody) // json String -> RequestBody
                 result match {
                   case Right(request) => {
-                    println(request.start)
                     try {
                       filter_data(reviewDs, request.start, request.end, request.limit, request.min_number_reviews)
                     } catch {
-                      case e: Exception => Json.obj("error" -> Json.fromString(e.getMessage())).noSpaces
+                      case e: Exception => e.getMessage()
                     }
                   }
-                  case Left(error) => Json.obj("error" -> Json.fromString(s"Error parsing JSON: $error")).noSpaces
+                  case Left(error) => s"Error parsing JSON: $error"
                 }
-              case _ => Json.obj("error" -> Json.fromString("please check your route")).noSpaces
+              case _ => "please check your route"
             }
-          case _ => Json.obj("error" -> Json.fromString("405 Method Not Allowed")).noSpaces
+          case _ => "405 Method Not Allowed"
         }
 
         exchange.getResponseHeaders().set("Content-Type", "application/json")
         exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length)
-        val os = exchange.getResponseBody
-        os.write(response.getBytes(StandardCharsets.UTF_8))
-        os.close()
+        val outputStream = exchange.getResponseBody
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8))
+        outputStream.close()
       }
     }
 
@@ -118,6 +115,6 @@ object ApiServer {
     server.setExecutor(null)
     server.start()
     println("Server is listening on port 8080")
-    StdIn.readLine("Press ENTER to stop the server\n")
+    StdIn.readLine("Press Ctrl + C to stop the server\n")
   }
 }
